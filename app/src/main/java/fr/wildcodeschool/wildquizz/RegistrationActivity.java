6b0daf60e091c861c0f55246704563789d338390
Hedi Benaissa <h_benaissa@hotmail.fr>
2018-04-13 15:00:08 +0200

    prise photo

--- app/src/main/java/fr/wildcodeschool/wildquizz/RegistrationActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildquizz/RegistrationActivity.java
@@ -1,14 +1,22 @@
 package fr.wildcodeschool.wildquizz;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private ImageView img ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,25 @@ public class RegistrationActivity extends AppCompatActivity {
                 RegistrationActivity.this.startActivity(gotoMenu);
             }
         });
+        img = findViewById(R.id.image_first);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
 
+            }
+
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(img);
+    }
+
 }
