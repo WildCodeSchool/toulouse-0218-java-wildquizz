ac6fee39a787430407f8f80bc00217f7293dfbb7
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-04-17 14:56:48 +0200

    authentification condition

--- app/src/main/java/fr/wildcodeschool/wildquizz/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildquizz/MainActivity.java
@@ -1,24 +1,79 @@
 package fr.wildcodeschool.wildquizz;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = FirebaseDatabase.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null){
+                    Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
         Button buttonConnexion = findViewById(R.id.button_connexion);
         buttonConnexion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gotoMenu = new Intent(MainActivity.this, MenuActivity.class);
-                MainActivity.this.startActivity(gotoMenu);
+                email = findViewById(R.id.edit_email);
+                password = findViewById(R.id.edit_password);
+
+                String emailText = email.getText().toString();
+                String passwordText = password.getText().toString();
+
+                if (emailText.isEmpty() || passwordText.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Veuillez entrer votre email et votre passe", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(emailText,passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Intent gotoMenu = new Intent(MainActivity.this, MenuActivity.class);
+                                MainActivity.this.startActivity(gotoMenu);
+                            }
+                        }
+                    });
+
+                }
             }
         });
 
@@ -32,4 +87,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+
+
+
+
+
 }
