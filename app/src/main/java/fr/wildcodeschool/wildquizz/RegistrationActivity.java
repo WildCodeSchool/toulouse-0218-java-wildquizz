0006a709b12d38664bb6f2de3392d5d940151b8c
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-04-17 21:51:42 +0200

    travailler sur la récup d'image

--- app/src/main/java/fr/wildcodeschool/wildquizz/RegistrationActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildquizz/RegistrationActivity.java
@@ -1,15 +1,20 @@
 package fr.wildcodeschool.wildquizz;
 
-import android.app.ActionBar;
+import android.*;
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,22 +27,25 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.common.hash.HashCode;
-import com.google.common.hash.Hashing;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
 
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
@@ -50,10 +58,14 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText mConfirmPassword;
     private EditText mEmail;
 
-    private Button buttonCreate;
+    private Button mButtonCreate;
+    private String child;
 
-    //private StorageReference mStorageRef;
-    //private Uri filePath;
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
+    private String mCurrentPhotoPath;
+    private Uri filePath;
+
 
 
     @Override
@@ -61,15 +73,19 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
         database = FirebaseDatabase.getInstance();
-        //mStorageRef = FirebaseStorage.getInstance().getReference();
 
         mAuth = FirebaseAuth.getInstance();
 
+        mProgress = new ProgressDialog(this);
+
+
         mEmail = findViewById(R.id.edit_email2);
 
-
         Intent register = getIntent();
+
 
         mImg = findViewById(R.id.image_first);
         mImg.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +93,8 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, 0);
+                //dispatchTakePictureIntent();
+
             }
         });
 
@@ -84,11 +102,11 @@ public class RegistrationActivity extends AppCompatActivity {
         mPassword = findViewById(R.id.edit_pass);
         mConfirmPassword = findViewById(R.id.edit_pass_confirm);
 
-        buttonCreate = findViewById(R.id.button_create);
-        buttonCreate.setOnClickListener(new View.OnClickListener() {
+        mButtonCreate = findViewById(R.id.button_create);
+        mButtonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String child = mUsername.getText().toString();
+                child = mUsername.getText().toString();
                 final String password = mPassword.getText().toString();
                 final String confirmPassword = mConfirmPassword.getText().toString();
                 final String email = mEmail.getText().toString();
@@ -121,20 +139,70 @@ public class RegistrationActivity extends AppCompatActivity {
                         }
                     });
                 }
-
             }
         });
 
-
-
     }
+
+    /*
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                //".jpg",         /* suffix */
+                //storageDir      /* directory */
+       // );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        //mCurrentPhotoPath = image.getAbsolutePath();
+        //return image;
+    //}
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(mImg);
+        mImg.setImageBitmap(bitmap);
+
+
+        /*if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK  && data != null) {
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mImg.setImageBitmap(bitmap);
+        }*/
     }
+
+    /*private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }*/
+
 
 
 
