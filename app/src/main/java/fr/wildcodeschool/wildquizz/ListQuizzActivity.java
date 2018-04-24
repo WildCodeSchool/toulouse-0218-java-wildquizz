ae2ac4dad9c89661590c3c1024b1caa7e9d2c76f
Hedi Benaissa <h_benaissa@hotmail.fr>
2018-04-24 14:51:57 +0200

    modiflistquizz

--- app/src/main/java/fr/wildcodeschool/wildquizz/ListQuizzActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildquizz/ListQuizzActivity.java
@@ -1,26 +1,43 @@
 package fr.wildcodeschool.wildquizz;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ListQuizzActivity extends AppCompatActivity {
+public class ListQuizzActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private ActionBarDrawerToggle mToggle;
+    private DrawerLayout mDrawerLayout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_quizz);
+
+        //Navigation Drawer :
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_list_quizz_created);
+        mToggle = new ActionBarDrawerToggle(ListQuizzActivity.this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Navigation View :
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_list_quizz_created);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ListView listView = findViewById(R.id.quizz_list);
 
@@ -56,5 +73,44 @@ public class ListQuizzActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.home) {
+            Intent goToHome = new Intent(this, MenuActivity.class);
+            this.startActivity(goToHome);
+        } else if (id == R.id.join) {
+            Intent goToJoin = new Intent(this, JoinQuizzActivity.class);
+            this.startActivity(goToJoin);
+        } else if (id == R.id.create) {
+            Intent goToCreate = new Intent(this, CreateQuizzActivity.class);
+            this.startActivity(goToCreate);
+        } else if (id == R.id.profile) {
+            Intent goToProfile = new Intent(this, ProfileActivity.class);
+            this.startActivity(goToProfile);
+        } else if (id == R.id.displayquizz) {
+            Intent goToDisplayQuizz = new Intent(this, DisplayQuizzActivity.class);
+            this.startActivity(goToDisplayQuizz);
+        } else if (id == R.id.listquizz) {
+            Intent goToListQuizz = new Intent(this, ListQuizzActivity.class);
+            this.startActivity(goToListQuizz);
+        } else if (id == R.id.logout) {
+            //Déconnexion
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
